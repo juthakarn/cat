d940841260f28df95b85b24a8320266b5e84768b
 juthakarn saelim <juthakarnsaelim@MacBook-Air-khxng-juthakarn.local>
2018-06-09 13:59:10 +0700

    navigation setup LoginScreen

---
+++ Component/LoginScreen.js
@@ -0,0 +1,11 @@
+import React ,{Component} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+class  LoginScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        )
+    }
+}
