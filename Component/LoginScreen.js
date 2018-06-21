8968296b481c888241896a12394dc58fd2ba132c
 juthakarn saelim <juthakarnsaelim@MacBook-Air-khxng-juthakarn.local>
2018-06-21 22:00:01 +0700

    navigation setup LoginScreen

--- Component/LoginScreen.js
+++ Component/LoginScreen.js
@@ -1,12 +1,39 @@
 import React ,{Component} from 'react';
 import { StyleSheet, Text, View} from 'react-native';
-class  LoginScreen extends Component{
+export default class  LoginScreen extends Component{
     render(){
         return(
-            <View>
-                <Text>Hello World</Text>
+            <View style={style.wrapper}>
+                <View style={style.titleWrapper}>
+                    <Text style={style.title}>Hello World</Text>
+                </View>
+                <View style={style.titleWrapper}>
+                    <Text style={style.subtitle}>A Little Princes</Text>
+                </View>
             </View>
-        )
+        );
     }
 }
-export default LoginScreen
+
+const styles = StyleSheet.create({
+    wrapper: {
+        backgroundColor: '#27ae60',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        color: 'white',
+        fontSize: 35,
+        fontWeight: 'bold'
+    },
+    subtitle: {
+        color: 'white',
+        fontWeight: '200',
+        paddingBottom: 20
+    },
+    titleWrapper: {
+        justifyContent: 'center',
+        flex: 1
+    }
+});
--- Router/router.js
+++ Router/router.js
@@ -6,21 +6,23 @@ export default createBottomTabNavigator(
     {
         Logoin: LoginScreen,
     },
-{
-    navigationOptions: ({ navigation }) => ({
-        tabBarIcon: ({ focused, tintColor }) => {
-            const { routeName } = navigation.state;
-            let iconName;
-            if(routeName === 'Login'){
-                iconName = `ios-log-in${focused ? '': '-outline'}`
-            }
-            return <Ionicons name={iconName} size={25} color={tintColor} />;
-        },
-    }),
-            tabBarOptions:{
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'Login') {
+                    iconName = `ios-log-in${focused ? '' : '-outline'}`;
+                }
+
+                // You can return any component that you like here! We usually use an
+                // icon component from react-native-vector-icons
+                return <Ionicons name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
             activeTintColor: 'tomato',
             inactiveTintColor: 'gray',
-            },
-
-}
+        },
+    }
 );
