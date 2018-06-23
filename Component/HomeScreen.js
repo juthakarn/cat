0c60523106e80260730bd1f6a1181919606b94c9
 juthakarn saelim <juthakarnsaelim@MacBook-Air-khxng-juthakarn.local>
2018-06-24 02:15:22 +0700

    navigation setup LoginScreen

--- Component/HomeScreen.js
+++ Component/HomeScreen.js
@@ -1,2 +1,39 @@
 import React ,{Component} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+export default class  HomeScreen extends Component{
+    render(){
+        return(
+            <View style={styles.wrapper}>
+                <View style={styles.titleWrapper}>
+                    <Text style={styles.title}>Hello World</Text>
+                </View>
+                <View>
+                    <Text style={styles.subtitle}>A Little Princes</Text>
+                </View>
+            </View>
+        );
+    }
+}
 
+const styles = StyleSheet.create({
+    wrapper: {
+        backgroundColor: '#27ae60',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        color: 'white',
+        fontSize: 35,
+        fontWeight: 'bold'
+    },
+    subtitle: {
+        color: 'white',
+        fontWeight: '200',
+        paddingBottom: 20
+    },
+    titleWrapper: {
+        justifyContent: 'center',
+        flex: 1
+    }
+});
--- Component/LoginScreen.js
+++ Component/LoginScreen.js
@@ -1,39 +1,45 @@
-import React ,{Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-export default class  LoginScreen extends Component{
+import React ,{ Component } from 'react';
+import { StyleSheet, View, Image, Text, KeyboardAvoidingView } from 'react-native';
+import LoginForm from './LoginForm';
+export default class LoginScreen extends Component {
     render(){
         return(
-            <View style={style.wrapper}>
-                <View style={style.titleWrapper}>
-                    <Text style={style.title}>Hello World</Text>
+            <KeyboardAvoidingView behavior="padding" style={styles.container}>
+                <View style={styles.logoContainer}>
+                    <Image
+                        style={styles.logo}
+                        source={require('../../image/logo.png')}
+                    />
+
+                    <Text style={styles.title}>Application for cats</Text>
                 </View>
-                <View style={style.titleWrapper}>
-                    <Text style={style.subtitle}>A Little Princes</Text>
+                <View style={styles.formContainer}>
+                <LoginForm/>
                 </View>
-            </View>
+            </KeyboardAvoidingView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        backgroundColor: '#27ae60',
+    container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        backgroundColor: '#3498db'
+    },
+    logoContainer: {
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent: 'center'
+    },
+    logo: {
+        width: 100,
+        height: 100
     },
     title: {
-        color: 'white',
-        fontSize: 35,
-        fontWeight: 'bold'
-    },
-    subtitle: {
-        color: 'white',
-        fontWeight: '200',
-        paddingBottom: 20
-    },
-    titleWrapper: {
-        justifyContent: 'center',
-        flex: 1
+        color: '#FFF',
+        marginTop: 10,
+        width: 160,
+        textAlign: 'center',
+        opacity: 0.9
     }
 });
