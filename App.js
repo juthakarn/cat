e36423a5f9d6a7ec77e5a077143e3c4ecfaeab20
 juthakarn saelim <juthakarnsaelim@MacBook-Air-khxng-juthakarn.local>
2018-12-06 02:41:27 +0700

    reduxstoreandlearningdiscard{pop,bar}

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk'
 import Router from './Router/router'
-export default class App extends React.Component {
-  render() {
-    return (
-      <Router/>
-    );
-  }
-}
+import reducers from './reducers'
+
+const createStoreAndapplyMiddleware = applyMiddleware(reduxThunk)(createStore)
+const store = createStoreAndapplyMiddleware(reducers)
+const App = () => (
+  <Provider store={store}>
+    <Router />
+  </Provider>
+)
+export default App
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <Router />
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
